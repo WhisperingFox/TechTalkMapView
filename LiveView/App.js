e543d7d0b9ce481333113feb566c5a57517f44b4
Lars Danz <danz@hm.edu>
2023-05-06 16:39:09 +0200

    map initial

--- LiveView/App.js
+++ LiveView/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import MapView from 'react-native-maps';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>GPS APP voll krass</Text>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 48.15506442863634,
+          longitude: 11.555812969194868,
+          latitudeDelta: 0.022,
+          longitudeDelta: 0.022,
+        }}
+      />
     </View>
   );
 }
@@ -16,5 +26,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: "90%",
+    height: "90%",
   },
 });
