6c3033761163c655e8df9f36df16696898f58c9d
Lars Danz <danz@hm.edu>
2023-05-08 16:27:57 +0200

    User Location

--- LiveView/App.js
+++ LiveView/App.js
@@ -1,9 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import MapView from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
 
 export default function App() {
+  
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.h1}>GPS APP mit Map</Text>
@@ -17,6 +43,7 @@ export default function App() {
           longitudeDelta: 0.022,
         }}
         // mapType={"satellite"}
+        showsUserLocation={true}
         
       />
     </View>
